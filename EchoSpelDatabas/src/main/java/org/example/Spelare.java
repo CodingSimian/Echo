8fef5b19c264415830ae3df0a597037a91c64af9
CodingSimian <viktor.bernas.ostrand@edu.edugrade.se>
2022-12-12 18:59:17 +0100

    La till peristence, spelar klassen samt ändrade i pom.xml

---
+++ EchoSpelDatabas/src/main/java/org/example/Spelare.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import javax.persistence.Entity;
+import java.io.Serializable;
+
+ //första entity klassen
+@Entity
+public class Spelare implements Serializable { //Serializable är viktigt för dataöverföring
+
+}
